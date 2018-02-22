3d69151990261dae5399f639f438af3314216822
farzana yeasmin <farzanayeasmin@obaidurmansair2.nvstu.edu>
2018-02-22 15:55:11 -0500

    first commit

---
+++ OddNum.java
@@ -0,0 +1,10 @@
+public class OddNum{
+public static void main(String[]args){
+
+for (int i=1; i <=100; i++){
+	if (i%2 !=0){
+System.out.println("Between 1 to 100 the odd numbers are:" + i);
+}
+}
+}
+}
